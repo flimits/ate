9d4a5004610d265a4357df7735f928e31e74c933
Jason Savoie <flimits@gmail.com>
2023-11-28 22:50:57 -0800

    updated webpack.config.js with appropriate plugins like htmlwebpackplugin, and package.json with proper start and build values

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,16 +20,34 @@ const initdb = async () =>
 // create a store - store name, 
 // put the store - content: {id, info }
 // await the request
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 // create an openDB - Database name, version.
 // create a transaction - database name, readonly,
-//c reate a store - store name,
+// create a store - store name,
 // get the store NOTE: WE DO NOT NEED TO GET ALL ONLY ONE - 1, 
 // await the request 
 // return the result
 
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async (id) => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  const jateDB = await openDB('jate',1);
+  const tx = jateDB.transaction('jate', 'readonly ');
+  const store = tx.objectStore('jate');
+  const request = store.get(id);
+  const result = await request;
+  console.log('result');
+  return result;
+}
 
 initdb();
